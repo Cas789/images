49f4261aee865065840b1fced13fd4c5e9a88680
Cas <kyle.bourke789@hotmail.com>
2020-08-05 20:28:11 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 #curl https://api.scpslgame.com/ip.php
-cd /home/container || exit
+echo "Server boot started"
+cd /home/container &&
+
+# Install Exiled
+echo "Installing Exiled"
+./Exiled.Installer-Linux -p /home/container/scp_server --pre-releases
+cd /home/container &&
 
 # Update Server
+echo "Checking for updates"
 if [ ! -z ${APPID} ]; then
 	./steamcmd.sh +login anonymous +force_install_dir /home/container/scp_server +app_update ${APPID} validate +quit
 fi
