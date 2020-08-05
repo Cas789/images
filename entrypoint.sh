3d14a7589f80222cbaa4635dcef10ab28a07fea1
Cas <kyle.bourke789@hotmail.com>
2020-08-05 15:51:10 +1000

    Added autoupdating, removed crap

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 #curl https://api.scpslgame.com/ip.php
-echo "Hello!"
 cd /home/container || exit
+
+# Update Server
+if [ ! -z ${APPID} ]; then
+	./steamcmd.sh +login anonymous +force_install_dir /home/container/scp_server +app_update ${APPID} validate +quit
+fi
+
+# Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo "/home/container/scp_server$: ${MODIFIED_STARTUP}"
-
-cd /home/container &&
-
-echo "Checking verkey.txt.."
-if [ ! -d "/home/container/.config/SCP Secret Laboratory" ];
-then mkdir "/home/container/.config/SCP Secret Laboratory";
-fi
-echo "Creating file.."
-touch "/home/container/.config/SCP Secret Laboratory/verkey.txt"
 
 cd /home/container/scp_server &&
 ${MODIFIED_STARTUP}
