dc96a57d768f5326107bc57ed0240b54e501bff6
Cas <kyle.bourke789@hotmail.com>
2020-08-05 20:48:11 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 #curl https://api.scpslgame.com/ip.php
-echo "Server boot started"
+cd /home/container
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo "/home/container/scp_server$: ${MODIFIED_STARTUP}"
+
+./Exiled.Installer-Linux -p /home/container/scp_server --pre-releases
 cd /home/container &&
 
-# Install Exiled
-echo "Installing Exiled"
-./Exiled.Installer-Linux --path /home/container/scp_server --pre-releases
-cd /home/container &&
-
-# Update Server
-echo "Checking for updates"
 if [ ! -z ${APPID} ]; then
 	./steamcmd.sh +login anonymous +force_install_dir /home/container/scp_server +app_update ${APPID} validate +quit
 fi
 
-# Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo "/home/container/scp_server$: ${MODIFIED_STARTUP}"
 
