1084976f61d0fac5dc61ac63c1ccefce06ee5051
Cas <kyle.bourke789@hotmail.com>
2020-08-05 14:54:50 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 #curl https://api.scpslgame.com/ip.php
 echo "Hello!"
-cd /home/container
+cd /home/container || exit
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo "/home/container/scp_server$: ${MODIFIED_STARTUP}"
 
-/home/container/scp_server/SCPSL_Data/Managed &&
 cd /home/container &&
+
+echo "Checking verkey.txt.."
+if [ ! -d "/home/container/.config/SCP Secret Laboratory" ];
+then mkdir "/home/container/.config/SCP Secret Laboratory";
+fi
+echo "Creating file.."
+touch "/home/container/.config/SCP Secret Laboratory/verkey.txt"
 
 cd /home/container/scp_server &&
 ${MODIFIED_STARTUP}
