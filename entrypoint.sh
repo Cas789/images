bfb94f465547c5433de04caf0128d984218d9604
Cas <kyle.bourke789@hotmail.com>
2020-08-06 13:38:53 +1000

    Exiled autoupdating

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #curl https://api.scpslgame.com/ip.php
-cd /home/container
+cd /home/container &&
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo "/home/container/scp_server$: ${MODIFIED_STARTUP}"
 
-./Exiled.Installer-Linux -p /home/container/scp_server --pre-releases
-cd /home/container &&
-
 if [ ! -z ${APPID} ]; then
 	./steamcmd.sh +login anonymous +force_install_dir /home/container/scp_server +app_update ${APPID} validate +quit
+fi
+
+if [ ${EXILED_UPDATE} == 1 ]; then
+	./Exiled.Installer-Linux -p /home/container/scp_server --pre-releases
 fi
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
