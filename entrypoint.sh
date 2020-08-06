492d629d5e830741f4c837a0fedf677c4d1e083f
Cas <kyle.bourke789@hotmail.com>
2020-08-06 13:58:33 +1000

    Fixes and cleaner bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 #curl https://api.scpslgame.com/ip.php
 cd /home/container &&
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
 echo "/home/container/scp_server$: ${MODIFIED_STARTUP}"
 
-if [ ! -z ${APPID} ]; then
-	./steamcmd.sh +login anonymous +force_install_dir /home/container/scp_server +app_update ${APPID} validate +quit
+if [[ -n ${APPID} ]]; then
+	./steamcmd.sh +login anonymous +force_install_dir /home/container/scp_server +app_update "${APPID}" validate +quit
 fi
 
-if [ ${EXILED_UPDATE} == 1 ]; then
+if [[ ${EXILED_UPDATE} == 1 ]]; then
 	./Exiled.Installer-Linux -p /home/container/scp_server --pre-releases
 fi
-
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo "/home/container/scp_server$: ${MODIFIED_STARTUP}"
 
 cd /home/container/scp_server &&
 ${MODIFIED_STARTUP}
