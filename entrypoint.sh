387bec9ee5f7a4eedd70343893242cc5a45e4249
Cas <kyle.bourke789@hotmail.com>
2020-08-06 15:04:33 +1000

    Removed debug and optionalised audo updating

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,12 @@ cd /home/container &&
 MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
 echo "/home/container/scp_server$: ${MODIFIED_STARTUP}"
 
-if [[ -n ${APPID} ]]; then
+if [[ -n ${APPID} ]] && [[ ${SERVER_UPDATE} == 1 ]]; then
 	echo "Updating SCP:SL..."
 	./steamcmd.sh +login anonymous +force_install_dir /home/container/scp_server +app_update "${APPID}" validate +quit
 	echo "Update Complete."
 fi
 
-echo "Debug: ${EXILED_UPDATE}"
 if [[ ${EXILED_UPDATE} == 1 ]]; then
 	echo "Updating Exiled..."
 	./Exiled.Installer-Linux -p /home/container/scp_server --pre-releases
