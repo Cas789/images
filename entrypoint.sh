b3183fa884d3bfa8a27f7bec97c6724f56f287ee
Cas <kyle.bourke789@hotmail.com>
2020-08-06 14:47:38 +1000

    Echoing status & debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,16 @@ MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/
 echo "/home/container/scp_server$: ${MODIFIED_STARTUP}"
 
 if [[ -n ${APPID} ]]; then
+	echo "Updating SCP:SL..."
 	./steamcmd.sh +login anonymous +force_install_dir /home/container/scp_server +app_update "${APPID}" validate +quit
+	echo "Update Complete."
 fi
 
+echo "Debug: ${EXILED_UPDATE}"
 if [[ ${EXILED_UPDATE} == 1 ]]; then
+	echo "Updating Exiled..."
 	./Exiled.Installer-Linux -p /home/container/scp_server --pre-releases
+	echo "Update Complete."
 fi
 
 cd /home/container/scp_server &&
