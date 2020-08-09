05541957fee3e411ef6f9ed191de30e725e3939b
Cas <kyle.bourke789@hotmail.com>
2020-08-09 20:35:02 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ cd /home/container &&
 MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
 echo "/home/container/$: ${MODIFIED_STARTUP}"
 
-cd /home/container &&
+cd /home/container/Bot &&
 ${MODIFIED_STARTUP}
