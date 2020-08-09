2879d2f7defc579c167727f353ed18d8c81c7b64
Cas <kyle.bourke789@hotmail.com>
2020-08-09 19:49:16 +1000

    Removed gameserver stuffs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 cd /home/container &&
 MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
-echo "/home/container/scp_server$: ${MODIFIED_STARTUP}"
+echo "/home/container/$: ${MODIFIED_STARTUP}"
 
-if [[ -n ${APPID} ]] && [[ ${SERVER_UPDATE} == 1 ]]; then
-	echo "Updating SCP:SL..."
-	./steamcmd.sh +login anonymous +force_install_dir /home/container/scp_server +app_update "${APPID}" validate +quit
-	echo "Update Complete."
-fi
-
-if [[ ${EXILED_UPDATE} == 1 ]]; then
-	echo "Updating Exiled..."
-	./Exiled.Installer-Linux -p /home/container/scp_server --pre-releases
-	echo "Update Complete."
-fi
-
-cd /home/container/scp_server &&
+cd /home/container &&
 ${MODIFIED_STARTUP}
