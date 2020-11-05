e0b6b98a86303de1eec9c3173bf2a9cfc016ae54
Cas <kyle.bourke789@hotmail.com>
2020-11-05 11:31:52 +1100

    Initial Commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 cd /home/container &&
 MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
-echo "/home/container/scp_server$: ${MODIFIED_STARTUP}"
+echo "/home/container/scpsl$: ${MODIFIED_STARTUP}"
 
 if [[ -n ${APPID} ]] && [[ ${SERVER_UPDATE} == 1 ]]; then
 	echo "Updating SCP:SL..."
-	./steamcmd.sh +login anonymous +force_install_dir /home/container/scp_server +app_update "${APPID}" validate +quit
+	./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container/scpsl +app_update "${APPID}" validate +quit
 	echo "Update Complete."
 fi
 
 if [[ ${EXILED_UPDATE} == 1 ]]; then
 	echo "Updating Exiled..."
-	./Exiled.Installer-Linux -p /home/container/scp_server --pre-releases
+	cd /home/container/scpsl &&
+	wget -O /home/container/scpsl/Exiled.Installer-Linux https://github.com/galaxy119/EXILED/releases/latest/download/Exiled.Installer-Linux
+	./Exiled.Installer-Linux -p /home/container/scpsl --pre-releases
 	echo "Update Complete."
 fi
 
-cd /home/container/scp_server &&
+if [[ ${MULTIADMIN_UPDATE} == 1 ]]; then
+	echo "Updating MultiAdmin..."
+	cd /home/container/scpsl &&
+	wget -O /home/container/scpsl/MultiAdmin.exe https://github.com/ServerMod/MultiAdmin/releases/latest/download/MultiAdmin.exe
+	echo "Update Complete."
+fi
+
+cd /home/container/scpsl &&
 ${MODIFIED_STARTUP}
